f658b8ccfc2aa96963df0da329004ae880061484
beatsky <631089634@qq.com>
2019-08-28 00:22:09 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HeaderNav from './common/Header/Header'
+
 function App() {
   return (
     <div className="App">
+      <HeaderNav />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      
     </div>
   );
 }
